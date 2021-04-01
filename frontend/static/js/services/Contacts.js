ea50558e01f04b12fcab3460562669875f66cd89
MichaelSilski <sil16005@byui.edu>
2021-03-31 20:29:07 -0600

    Added form for sending emails

--- frontend/static/js/services/Contacts.js
+++ frontend/static/js/services/Contacts.js
@@ -4,15 +4,29 @@ export default class Contacts {
       this.lastName = "";
       this.subject = "";
       this.message = "";
+      this.emailTo = "";
+      this.emailFrom = "";
+      this.emailSubject = "";
+      this.emailMessage = "";
     }
 
-    onSubmit() {
+    onFeedbackSubmit() {
         document.getElementById('submit').addEventListener('click', function () {
             let firstName = document.getElementById('firstName').innerHTML;
             let lastName = document.getElementById('lastName').innerHTML;
             let subject = document.getElementById('subject').innerHTML;
             let message = document.getElementById('message').innerHTML;
-            //send information to backend or to specific email
+            //send information to backend
+        });
+    }
+
+    onEmailSubmit() {
+        document.getElementById('emailSubmit').addEventListener('click', function () {
+            let emailTo = document.getElementById('emailTo').innerHTML;
+            let emailFrom = document.getElementById('emailFrom').innerHTML;
+            let emailSubject = document.getElementById('emailSubject').innerHTML;
+            let emailMessage = document.getElementById('emailMessage').innerHTML;
+            //send information to backend/specific email
         });
     }
 }

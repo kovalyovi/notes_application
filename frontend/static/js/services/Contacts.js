9d603fd097ba65b3e541dc13ba7c3bebb2288617
MichaelSilski <sil16005@byui.edu>
2021-03-31 20:15:10 -0600

    Created contact page. Added missing notes page.

---
+++ frontend/static/js/services/Contacts.js
@@ -0,0 +1,18 @@
+export default class Utils {
+    constructor() {
+      this.firstName = "";
+      this.lastName = "";
+      this.subject = "";
+      this.message = "";
+    }
+
+    onSubmit() {
+        document.getElementById('submit').addEventListener('click', function () {
+            let firstName = document.getElementById('firstName').innerHTML;
+            let lastName = document.getElementById('lastName').innerHTML;
+            let subject = document.getElementById('subject').innerHTML;
+            let message = document.getElementById('message').innerHTML;
+            //send information to backend or to specific email
+        });
+    }
+}
